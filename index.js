587047c4b11827c29370a6aae71d51862c9c2782
--unset <nate@konimbo.co.il>
2017-02-23 17:41:27 +0200

    heroku config

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+var users = [],
+    connections = [];
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/connections', (req, res) => {
+    res.send({
+        connections: connections.length
+    })
+})
+
+io.sockets.on('connection', (socket) => {
+    // create connection
+    connections.push(socket)
+
+    // disconnect
+    socket.on('disconnect', () => {
+        connections.splice(connections.indexOf(socket), 1)
+    })
+
+    // send message
+    socket.on('send message', (msgData) => {
+        io.sockets.emit('new message', {msg: msgData})
+    })
+})
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), () => {
+    console.log("Node app is running; http://localhost:" + app.get('port'))
+});
